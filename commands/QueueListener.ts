133ad7a82521528715fecfda360bc244af700c10
Romain Lanz <romain.lanz+github@pm.me>
2023-01-03 13:28:25 +0100

    refactor(commands): add description to queue:listen

--- commands/QueueListener.ts
+++ commands/QueueListener.ts
@@ -9,7 +9,7 @@ import { BaseCommand, flags } from '@adonisjs/core/build/standalone';
 
 export default class QueueListener extends BaseCommand {
 	public static commandName = 'queue:listen';
-	public static description = '';
+	public static description = 'Listen to one or multiple queues';
 
 	@flags.array({ alias: 'q', description: 'The queue(s) to listen on' })
 	public queue: string[] = [];
@@ -21,14 +21,16 @@ export default class QueueListener extends BaseCommand {
 
 	public async run() {
 		const { Queue } = this.application.container.resolveBinding('Setten/Queue');
-		const Config = this.application.container.resolveBinding('Adonis/Core/Config')
+		const Config = this.application.container.resolveBinding('Adonis/Core/Config');
 
-		if (this.queue.length === 0) this.queue = Config.get('queue').queueNames
+		if (this.queue.length === 0) this.queue = Config.get('queue').queueNames;
 
 		await Promise.all(
-      this.queue.map(queue => Queue.process({
-        queueName: queue,
-      }))
-    )
+			this.queue.map((queue) =>
+				Queue.process({
+					queueName: queue,
+				})
+			)
+		);
 	}
 }
